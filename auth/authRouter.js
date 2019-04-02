8cf972766f17c9bddca0874178ed26a93ea4ae31
Noah <noah.stoeckel@gmail.com>
2019-04-01 23:39:11 -0400

    adds users and authentication routers

---
+++ auth/authRouter.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const users = require("../users-folder/usersModel");
+
+server.post("/register", (req, res) => {});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+const userRoutes = require("./users-folder/userRoutes");
+server.use("/api/users", userRoutes);
+const authRoutes = require("./auth/authRouter");
+server.use("/api/auth", authRoutes);
+
 server.get("/", (req, res) => {
   res.status(200).json({ message: "please use API endpoints" });
 });

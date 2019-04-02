81b4f0f31a18af016b4846b8cdb12f32fd6bd335
Noah <noah.stoeckel@gmail.com>
2019-04-02 09:06:31 -0400

    add generation token  function

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,7 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const users = require("../users-folder/usersModel");
+const jwt = require("jsonwebtoken");
+
+const secret = process.env.JWT_SECRET || "secret";
+
+function generateToken(user) {
+  //construct a payload
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    department: user.department
+  };
+
+  //add options
+  const options = {
+    expiresIn: "1d"
+  };
+
+  //generate a token with based on the payload the secret and the options
+  return jwt.sign(payload, secret, options);
+}
 
 router.post("/register", (req, res) => {
   //get credentials off body
@@ -29,7 +50,8 @@ router.post("/login", (req, res) => {
     .then(user => {
       console.log(user);
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `welcome ${user.username}` });
+        const token = generateToken(user);
+        res.status(200).json({ message: `welcome ${user.username}`, token });
       }
     })
     .catch(error => res.status(500).json({ error }));

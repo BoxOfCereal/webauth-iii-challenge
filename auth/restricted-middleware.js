3d20c5890629e6f3c726c4c242c9c12567f2ce35
Noah <noah.stoeckel@gmail.com>
2019-04-02 09:15:20 -0400

    add users route

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,19 +1,5 @@
-require("dotenv").config();
-const secret = process.env.JWT_SECRET || "secret";
 
-function generateToken(user) {
-  //construct a payload
-  const payload = {
-    subject: user.id,
-    username: user.username,
-    department: user.department
-  };
 
-  //add options
-  const options = {
-    expiresIn: "1d"
-  };
-
-  //generate a token with based on the payload the secret and the options
-  jwt.sign(payload, secret, options);
+module.exports = function(req,res,next){
+  const 
 }
--- users-folder/userRouter.js
+++ users-folder/userRouter.js
@@ -2,4 +2,13 @@ const express = require("express");
 const router = express.Router();
 const users = require("./usersModel");
 
+router.get("/", (req, res) => {
+  users
+    .find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => res.status(500).json({ error }));
+});
+
 module.exports = router;
